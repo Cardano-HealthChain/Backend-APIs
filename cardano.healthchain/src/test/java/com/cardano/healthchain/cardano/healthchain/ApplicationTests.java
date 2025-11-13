91f3b9e26cc07756254d40b09f6ae39944f14c84
Azu Chukwuemeka <azuchukwuemeka9@gmail.com>
2025-11-13 13:10:43 -0800

    Initial Setup for Backend API
    Added The Necessary files to begin cardano.healthchain backend

---
+++ cardano.healthchain/src/test/java/com/cardano/healthchain/cardano/healthchain/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardano.healthchain.cardano.healthchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
