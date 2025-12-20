85a22b20ffe313497da8e592a51c6f7caf05e96e
Azu Chukwuemeka <azuchukwuemeka9@gmail.com>
2025-12-20 04:01:47 -0800

    Removed UserId from MedicalDataUploadRequest cause it's passed as requst parameter

--- cardano.healthchain/src/main/java/com/cardano/healthchain/cardano/healthchain/utils/medicalData/dtos/MedicalDataUploadRequest.java
+++ cardano.healthchain/src/main/java/com/cardano/healthchain/cardano/healthchain/utils/medicalData/dtos/MedicalDataUploadRequest.java
@@ -3,26 +3,17 @@ package com.cardano.healthchain.cardano.healthchain.utils.medicalData.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public class MedicalDataUploadRequest {
-    @NotBlank(message = "User identifier is required")
-    private String user_id;
     @NotBlank(message = "Record type is required")
     private String record_type;
     @NotBlank(message = "Record data is required")
     private String record_data;
     private String diagnosis;
-    public MedicalDataUploadRequest(String user_id, String record_type, String record_data, String diagnosis) {
-        this.user_id = user_id;
+    public MedicalDataUploadRequest(String record_type, String record_data, String diagnosis) {
         this.record_type = record_type;
         this.record_data = record_data;
         this.diagnosis = diagnosis;
     }
     public MedicalDataUploadRequest() {
-    }
-    public String getUser_id() {
-        return user_id;
-    }
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
     public String getRecord_type() {
         return record_type;
